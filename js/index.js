9645f99ee858abca00ea82b7b48cf72d98b04941
明远湖之鱼 <488548816@qq.com>
2023-03-30 00:04:39 +0800

    加上《长津湖》电影色彩图片

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
 let infoList = document.querySelectorAll('.contrastBtn');
-const megs = ['empty_shot','motion','empty_shot','motion'];
+const megs = ['empty_shot','motion','color','motion'];
 for(let i = 0;i<infoList.length;i++){
-    infoList[i].addEventListener("click",function(){
-        //路径+参数名+参数值  使用encodeURI进行编码，可以传中文
-        window.open(encodeURI('./src/pages/compare.html?'+'message='+megs[i]))
-    })
+    if(i!=2){
+        infoList[i].addEventListener("click",function(){
+            //路径+参数名+参数值  使用encodeURI进行编码，可以传中文
+            window.open(encodeURI('./src/pages/compare.html?'+'message='+megs[i]))
+        })
+    }else{
+        infoList[i].addEventListener("click",function(){
+            //路径+参数名+参数值  使用encodeURI进行编码，可以传中文
+            window.open(encodeURI('./src/pages/ChangjinLake.html?'+'message='+megs[i]))
+        })
+    }
 }
