de2d5a18580c28a569aeae3587c3996097054511
明远湖之鱼 <488548816@qq.com>
2023-04-03 21:13:22 +0800

    网页更改，内嵌ifame

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 let infoList = document.querySelectorAll('.contrastBtn');
 const megs = ['color_motion','empty_shot','red_image'];
 for(let i = 0;i<infoList.length;i++){
-    if(i!=2){
+    if(i==0){
         infoList[i].addEventListener("click",function(){
             //路径+参数名+参数值  使用encodeURI进行编码，可以传中文
-            window.open(encodeURI('./src/pages/compareSite/compare.html?'+'message='+megs[i]))
+            window.open(encodeURI('./src/pages/color_motion/compare.html?'+'message='+megs[i]))
+        })
+    }else if(i==1){
+        infoList[i].addEventListener("click",function(){
+            //路径+参数名+参数值  使用encodeURI进行编码，可以传中文
+            window.open(encodeURI('./src/pages/empty_shot/compare.html?'+'message='+megs[i]))
         })
     }else{
         infoList[i].addEventListener("click",function(){
@@ -12,5 +17,5 @@ for(let i = 0;i<infoList.length;i++){
             window.open(encodeURI('./src/pages/red_image/red_image.html?'+'message='+megs[i]))
         })
     }
-
 }
+
